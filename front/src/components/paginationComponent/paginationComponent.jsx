8f58a26de97f9419c304ae5e67336c3434b16fb9
rubencorba <rubencorba@gmail.com>
2024-11-25 18:10:34 -0300

    estilos paginator

--- front/src/components/paginationComponent/paginationComponent.jsx
+++ front/src/components/paginationComponent/paginationComponent.jsx
@@ -17,7 +17,7 @@ function PaginationComponent() {
     dispatch(updatePage(p))
   }
   const handleNext =()=>{
-    /* if (currentPagina==1) return */
+    
     const p= currentPagina+1
     dispatch(updatePage(p))
   }
@@ -25,121 +25,108 @@ function PaginationComponent() {
   
     
     return (
-        <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
-        <div className="flex flex-1 justify-between sm:hidden">
-          <a
-            href="#"
-            className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
-          >
-            Previous
-          </a>
-          <a
-            href="#"
-            className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
-          >
-            Next
-          </a>
-        </div>
-        <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
+        <div className="flex items-center justify-center  px-4 py-6">
+        
+        
           {/* <div>
             <p className="text-sm text-gray-700">
               Showing <span className="font-medium">1</span> to <span className="font-medium">10</span> of{' '}
               <span className="font-medium">97</span> results
             </p>
           </div> */}
-          <div>
-            <nav aria-label="Pagination" className="isolate inline-flex -space-x-px rounded-md shadow-sm">
+          
+            <div  className=" inline-flex -space-x-px  shadow-sm bg-white border-2 border-[#0477AD]">
               <button onClick={()=>handlePrev()}>
-              <a
-                /* href="#" */
-                className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+              <div
+                
+                className="relative inline-flex items-center  px-2 py-2 text-gray-400 border-r border-1 border-[#0477AD] hover:bg-gray-50 focus:z-20 focus:outline-offset-0 "
               >
                 <span className="sr-only">Previous</span>
-                <ChevronLeftIcon aria-hidden="true" className="h-5 w-5" />
-              </a>
+                <ChevronLeftIcon aria-hidden="true" className="h-5 w-5 text-[#0477AD]" />
+              </div>
               </button>
               {/* Current: "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" */}
-              <button onClick={()=>handlePage(1)}>
-              <a
-                /* href="#" */
-                aria-current="page"
+              <button onClick={()=>handlePage(1)} >
+              <div
+                
+                
                 className={`
                  ${
-                  currentPagina === 1 ? "relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" : "relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                  currentPagina === 1 ? "border-1 border-[#0477AD]   inline-flex items-center bg-[#0477AD] px-4 py-2 text-sm font-semibold text-white focus:z-20  " : "text-[#0477AD] border-r border-1 border-[#0477AD] relative inline-flex items-center px-4 py-2 text-sm font-semibold   "
                 }`}
               >
                 1
-              </a>
+              </div>
               </button>
               <button onClick={()=>handlePage(2)}>
-              <a
-                /* href="#" */
+              <div
+                
                 className={`
                  ${
-                  currentPagina === 2 ? "relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" : "relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                  currentPagina === 2 ? "border-1 border-[#0477AD]   inline-flex items-center bg-[#0477AD] px-4 py-2 text-sm font-semibold text-white focus:z-20  " : "text-[#0477AD] border-r border-1 border-[#0477AD] relative inline-flex items-center px-4 py-2 text-sm font-semibold"
                 }`}
               >
                 2
-              </a>
+              </div>
               </button>
               <button onClick={()=>handlePage(3)}>
-              <a
-                /* href="#" */
+              <div
+                
                 className={`
                  ${
-                  currentPagina === 3 ? "relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" : "relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                  currentPagina === 3 ? "border-1 border-[#0477AD]   inline-flex items-center bg-[#0477AD] px-4 py-2 text-sm font-semibold text-white focus:z-20  " : "text-[#0477AD] border-r border-1 border-[#0477AD] relative inline-flex items-center px-4 py-2 text-sm font-semibold"
                 }`}
               >
                 3
-              </a>
+              </div>
               </button>
               <button onClick={()=>handlePage(4)}>
-              <a
-                /* href="#" */
+              <div
+                
                 className={`
                  ${
-                  currentPagina === 4 ? "relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" : "relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                  currentPagina === 4 ? "border-1 border-[#0477AD]   inline-flex items-center bg-[#0477AD] px-4 py-2 text-sm font-semibold text-white focus:z-20  " : "text-[#0477AD] border-r border-1 border-[#0477AD] relative inline-flex items-center px-4 py-2 text-sm font-semibold"
                 }`}
               >
                 4
-              </a>
+              </div>
               </button>
               <button onClick={()=>handlePage(5)}>
-              <a
-                /* href="#" */
+              <div
+                
                 className={`
                  ${
-                  currentPagina === 5 ? "relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" : "relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                  currentPagina === 5 ? "border-1 border-[#0477AD]   inline-flex items-center bg-[#0477AD] px-4 py-2 text-sm font-semibold text-white focus:z-20  " : "text-[#0477AD] border-r border-1 border-[#0477AD] relative inline-flex items-center px-4 py-2 text-sm font-semibold "
                 }`}
               >
                 5
-              </a>
+              </div>
               </button>
               <button onClick={()=>handlePage(6)}>
-              <a
-                /* href="#" */
+              <div
+                
                 className={`
                  ${
-                  currentPagina === 6 ? "relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" : "relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                  currentPagina === 6 ? "border-1 border-[#0477AD]   inline-flex items-center bg-[#0477AD] px-4 py-2 text-sm font-semibold text-white focus:z-20  " : "text-[#0477AD] border-r border-1 border-[#0477AD] relative inline-flex items-center px-4 py-2 text-sm font-semibold"
                 }`}
               >
                 6
-              </a>
+              </div>
               </button>
               
               
               <button onClick={()=>handleNext()}>
-              <a
-                /* href="#" */
-                className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+              <div
+                
+                className="relative inline-flex items-center  px-2 py-2 text-gray-400 border-l border-1 border-[#0477AD] hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
               >
                 <span className="sr-only">Next</span>
-                <ChevronRightIcon aria-hidden="true" className="h-5 w-5" />
-              </a>
+                <ChevronRightIcon aria-hidden="true" className="h-5 w-5 text-[#0477AD]" />
+              </div>
               </button>
-            </nav>
-          </div>
-        </div>
+            </div>
+          
+        
       </div>
 
        
