13492e253c972c2db4007bd886a6134ddb6ef34d
rubencorba <rubencorba@gmail.com>
2024-12-10 18:03:52 -0300

    comenzando con la landing

--- front/src/components/navbar/navbarComponent.jsx
+++ front/src/components/navbar/navbarComponent.jsx
@@ -15,24 +15,29 @@ function Navbar() {
     <div style={{
         width: '100%',
         height: '100%',
-        /* justifyContent: 'flex-start', */
-        /* justifyContent: 'space-between', */
-        /* alignItems: 'flex-start', */
         alignItems: 'center',
         display: 'flex',
         
         }}>      
-        {['inicio', 'vehiculos', 'usuarios', 'salir'].map((path, index, array) => (
+        {['inicio', 'vehiculos', 'usuarios', 'salir'].map((path, index, array) => {
+        // Determina si la ruta actual coincide con el enlace
+        const isActive = location.pathname === `/${path}`;
+        
+        
+        return (
         <Link to={`/${path}`} key={index} style={{ flex: 1 }}>
-          <div className={`h-[105px] py-5 bg-[#0477AD] flex justify-center items-center box-border
-           ${index !== array.length - 1 ? 'border-r border-white' : ''}`}
+          <div className={`h-[105px] py-5 ${
+                isActive ? 'bg-[#0a5477]' : 'bg-[#0477AD]'
+              } flex justify-center items-center box-border ${
+                index !== array.length - 1 ? 'border-r border-white' : ''
+              }`}
            >
             <div class="text-white text-[1.5rem] font-inter font-bold break-words">
               {path.charAt(0).toUpperCase() + path.slice(1)}
             </div>
           </div>
         </Link>
-      ))}
+      )})}
     </div>
 
 
@@ -42,7 +47,3 @@ function Navbar() {
 
 export default Navbar;
 
-/* Icono salir */
-{/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-<path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9" />
-</svg> */}
--- front/src/views/landing/landingComponent.jsx
+++ front/src/views/landing/landingComponent.jsx
@@ -2,7 +2,8 @@ import { useNavigate } from 'react-router-dom';
 
 import { useEffect, useState } from 'react';
 
-import snLogo from './sn-logo.png'
+import snLogo from './sn-logo-blanco.png'
+import snFondo from './sn-imagen-fondo.jpeg'
 import { useDispatch } from 'react-redux';
 import { postUsers, updateCurrentUser } from '../../redux/actions';
 
@@ -64,26 +65,88 @@ function Landing() {
 
 
   return (
-    <div class="flex min-h-full flex-col justify-center px-6 py-8 lg:px-8">
+    <div className="w-full h-screen  flex flex-col justify-start items-center gap-[10rem]">
+    {/* Encabezado */}
+    <div className="w-full h-[5rem]  py-[1rem] bg-[#0477ad] shadow flex items-center">
+      <div className="relative w-full h-full justify-start items-center flex">
+        <img src={snLogo} alt="Logo San Nicolas" className="w-[3rem] ml-[1.5rem]"/>
+        <div className=" w-[39px] border border-[#f6f5f5]  rotate-90" />
+        <div className=" text-[#f6f5f5] text-sm font-normal font-inter">
+          Corralón San Nicolás
+        </div>
+      </div>
+    </div>
+
+    <img src={snFondo} alt="" className="absolute w-full h-full object-cover top-0 left-0 -z-10"/>
+
+    {/* Contenedor principal */}
+    <div className="w-full max-w-md px-8 py-10 bg-white rounded-lg shadow flex flex-col justify-center items-center gap-1">
+      <div className="h-[317px] flex flex-col justify-center items-start gap-10">
+        {/* Título */}
+        <div className="text-[#3d4245] text-2xl font-bold font-inter">
+          Inicio de sesión
+        </div>
+
+        {/* Formulario */}
+        <div className="flex flex-col justify-start items-start gap-6">
+          {/* Campo DNI */}
+          <div className="h-[75px] flex flex-col justify-start items-start gap-2">
+            <label className="text-[#3d4245] text-sm font-normal font-inter">
+              DNI
+            </label>
+            <div className="self-stretch h-[50px] p-2 rounded-md border border-[#687073] flex items-center gap-1">
+              <input
+                type="text"
+                placeholder="0000000000000"
+                className="w-full text-[#a3b8c1] text-sm font-normal font-inter outline-none"
+              />
+            </div>
+          </div>
+
+          {/* Campo Contraseña */}
+          <div className="h-[75px] flex flex-col justify-start items-start gap-2">
+            <label className="text-[#3d4245] text-sm font-normal font-inter">
+              Contraseña
+            </label>
+            <div className="self-stretch h-[50px] pl-2 pr-3 py-2 rounded-md border border-[#687073] flex justify-between items-center">
+              <input
+                type="password"
+                placeholder="Contraseña"
+                className="w-full text-[#a3b8c1] text-sm font-normal font-inter outline-none"
+              />
+              <div className="w-5 h-5" />
+            </div>
+          </div>
+
+          {/* Botón */}
+          <button className="w-full h-[50px] px-[18px] py-[13px] bg-[#0477ad] rounded-lg flex justify-center items-center gap-1">
+            <span className="text-[#f6f5f5] text-base font-semibold font-inter">
+              Iniciar sesión
+            </span>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+  );
+}
+
+export default Landing;
+
+
+/*  <div class="flex min-h-full flex-col justify-center px-6 py-8 lg:px-8">
   <div class="sm:mx-auto sm:w-full sm:max-w-sm">
     <img class="mx-auto  w-auto" src={snLogo} alt="Your Company"/>
     <h2 class="mt-8 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Ingreso a tu cuenta</h2>
   </div>
 
   <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-    <form class="space-y-6" /* action="#" */ /* method="POST" */ onSubmit={handleSubmit}>
+    <form class="space-y-6" onSubmit={handleSubmit}>
       <div>
-        <label /* for="email" */ class="block text-sm font-medium leading-6 text-gray-900">Usuario</label>
+        <label class="block text-sm font-medium leading-6 text-gray-900">Usuario</label>
         <div class="mt-2">
           <input
-           /* id="email" */
-           /* name="email" */
-           /* type="email"  */
-           /* autocomplete="email" */ 
-
-           /* Luego descomentar required para que sea un campo requerido!!!! */
-
-           /* required  */class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+           class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
            
            value={userData.usuario} name='usuario' onChange={handleChange}/>
         </div>
@@ -91,21 +154,14 @@ function Landing() {
 
       <div>
         <div class="flex items-center justify-between">
-          <label /* for="password" */ class="block text-sm font-medium leading-6 text-gray-900">Contraseña</label>
+          <label class="block text-sm font-medium leading-6 text-gray-900">Contraseña</label>
           <div class="text-sm">
-            <a /* href="#" */ class="font-semibold text-indigo-600 hover:text-indigo-500">Olvidaste tu contraseña?</a>
+            <a  class="font-semibold text-indigo-600 hover:text-indigo-500">Olvidaste tu contraseña?</a>
           </div>
         </div>
         <div class="mt-2">
           <input 
-            /* id="password" 
-            name="password" 
-            type="password" */ 
-            /* autocomplete="current-password" */ 
-
-            /* Luego descomentar required para que sea un campo requerido!!!! */
-
-            /* required */ class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+             class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             
             value={userData.contraseña} name='contraseña' onChange={handleChange}/>
         </div>
@@ -126,18 +182,6 @@ function Landing() {
       No estás registrado?
       <a  class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Crea una cuenta</a>
     </p>
-    {/* <p class="mt-6 text-center text-sm text-gray-500">
-      Usuario1: usuario | Contraseña: 123asd
-    </p>
-    <p class="mt-3 text-center text-sm text-gray-500">
-      Usuario2: usuario2 | Contraseña: 123asd
-    </p>
-    <p class="mt-3 text-center text-sm text-gray-500">
-      Administrador: admin | Contraseña: 123asd
-    </p> */}
+    
   </div>
-</div>
-  );
-}
-
-export default Landing;
+</div> */
