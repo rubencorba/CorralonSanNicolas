f3849bbf7f688f947b843b1a0bed50b8eef99b62
rubencorba <rubencorba@gmail.com>
2025-01-11 17:34:48 -0300

    vinculando infracciones a postSecuestros

--- back/src/controllers/secuestrosController.js
+++ back/src/controllers/secuestrosController.js
@@ -1,61 +1,101 @@
-const {getAllSecuestros}=require('../handlerss/getAllSecuestros');
-const { infraccionesBySecuestroId } = require('../handlerss/infraccionesBySecuestroId');
-const { postSecuestro } = require('../handlerss/postSecuestro');
-const { secuestroDetailById } = require('../handlerss/secuestroDetailById');
-const { validateUniqueInventario } = require('../handlerss/validateUniqueInventario');
+const { getAllSecuestros } = require("../handlerss/getAllSecuestros");
+const {
+  infraccionesBySecuestroId,
+} = require("../handlerss/infraccionesBySecuestroId");
+const { postSecuestro } = require("../handlerss/postSecuestro");
+const { secuestroDetailById } = require("../handlerss/secuestroDetailById");
+const {
+  validateUniqueInventario,
+} = require("../handlerss/validateUniqueInventario");
 
-const getAllSecuestrosController= async (req,res)=>{
-    const {page = 1} =req.query
+const getAllSecuestrosController = async (req, res) => {
+  const { page = 1 } = req.query;
 
-    const options={
-        limit: 9,
-        offset: (page-1)*9
-    }
+  const options = {
+    limit: 9,
+    offset: (page - 1) * 9,
+  };
 
-    const response=await getAllSecuestros(options);
+  const response = await getAllSecuestros(options);
+  res.status(200).json(response);
+};
+const getDetailSecuestroController = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const response2 = await infraccionesBySecuestroId(id);
+    let response = await secuestroDetailById(id);
+
+    response = response.toJSON();
+
+    response.infracciones = response2;
     res.status(200).json(response);
-}
-const getDetailSecuestroController= async (req,res)=>{
-    const {id}=req.params;
-
-    try {
-        const response2= await infraccionesBySecuestroId(id)
-        let response=await secuestroDetailById(id);
-
-        response = response.toJSON();
-
-        response.infracciones=response2
-        res.status(200).json(response);
-    } catch (error) {
-        res.status(400).json({error:error.message});
-    }
-}
-const postSecuestroController= async (req,res)=>{
-    const {infractorCuil,infractorDni,vehiculoDominio,vehiculoMarca,vehiculoModelo,infractorNombre,infractorSexo,vehiculoTipo,fecha_hora,nroInventario,sector,foto}=req.body;
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+const postSecuestroController = async (req, res) => {
+  const {
+    infractorCuil,
+    infractorDni,
+    vehiculoDominio,
+    vehiculoMarca,
+    vehiculoModelo,
+    infractorNombre,
+    infractorSexo,
+    vehiculoTipo,
+    fecha_hora,
+    nroInventario,
+    sector,
+    foto,
+    actaNro,
+    actaInspector,
+    actaLugar,
+    actaFecha_hora,
+    infracciones,
+  } = req.body;
+  /* infracciones, */
+  try {
+    const response = await postSecuestro(
+      infractorCuil,
+      infractorDni,
+      vehiculoDominio,
+      vehiculoMarca,
+      vehiculoModelo,
+      infractorNombre,
+      infractorSexo,
+      vehiculoTipo,
+      fecha_hora,
+      nroInventario,
+      sector,
+      foto,
+      actaNro,
+      actaInspector,
+      actaLugar,
+      actaFecha_hora,
+      infracciones
+    );
     /* infracciones, */
-    try {
-        const response= await postSecuestro(infractorCuil,infractorDni,vehiculoDominio,vehiculoMarca,vehiculoModelo,infractorNombre,infractorSexo,vehiculoTipo,fecha_hora,nroInventario,sector,foto);
-        /* infracciones, */
-        res.status(200).json(response);
-    } catch (error) {
-        res.status(400).json({error:error.message});
-    }
-}
-const validateUniqueInventarioController= async (req,res)=>{
-    const {nroInventario}=req.params;
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+const validateUniqueInventarioController = async (req, res) => {
+  const { nroInventario } = req.params;
 
-    try {
-        const response= await validateUniqueInventario(nroInventario)
+  try {
+    const response = await validateUniqueInventario(nroInventario);
 
-        res.status(200).json(response);
-    } catch (error) {
-        res.status(400).json({error:error.message});
-    }
-}
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
-module.exports={
-    getAllSecuestrosController,
-    getDetailSecuestroController,
-    postSecuestroController,
-    validateUniqueInventarioController
-    };
+module.exports = {
+  getAllSecuestrosController,
+  getDetailSecuestroController,
+  postSecuestroController,
+  validateUniqueInventarioController,
+};
--- back/src/handlerss/postSecuestro.js
+++ back/src/handlerss/postSecuestro.js
@@ -1,4 +1,4 @@
-const { Secuestros, Vehiculos,Infractores } = require("../db.js");
+const { Secuestros, Vehiculos,Infractores,Actas,Infracciones,Secuestros_infracciones } = require("../db.js");
 
 const postSecuestro = async (
   infractorCuil,
@@ -12,11 +12,19 @@ const postSecuestro = async (
   fecha_hora,
   nroInventario,
   sector,
-  foto
+  foto,
+  actaNro,
+  actaInspector,
+  actaLugar,
+  actaFecha_hora,
+  infracciones
 ) => {
   const ingreso = "2345";
 
   var fechaTimestamp = new Date(fecha_hora).getTime();
+
+  const isoDateString = actaFecha_hora.replace(" ", "T");
+  const actaDate = new Date(isoDateString).getTime();
 
   try {
     const newSecuestro = await Secuestros.create({
@@ -40,9 +48,56 @@ const postSecuestro = async (
       sexo: infractorSexo,
       cuil: infractorCuil,
     });
+
+    const newActa = await Actas.create({
+      nro: actaNro,
+      inspector: actaInspector,
+      lugar: actaLugar,
+      fecha_hora: actaDate,
+    });
     console.log(Object.keys(newSecuestro.__proto__));
     await newSecuestro.setVehiculo(newVehiculo);
     await newSecuestro.setInfractore(newInfractor);
+    await newSecuestro.setActa(newActa);
+
+
+    const findMatchingInfraccion = async (descrip, digesto) => {
+      const allInfracciones = await Infracciones.findAll({
+        where: { digesto },
+      });
+    
+      for (const infraccion of allInfracciones) {
+        const dbWords = infraccion.descrip.split(/\s+/);
+        const inputWords = descrip.split(/\s+/);
+    
+        const commonWords = dbWords.filter((word) => inputWords.includes(word));
+        if (commonWords.length >= 4) {
+          return infraccion; // Coincidencia aceptable encontrada
+        }
+      }
+    
+      return null; // No se encontró coincidencia
+    };
+
+     // Vincular infracciones
+     for (const infraccion of infracciones) {
+      const matchingInfraccion = await findMatchingInfraccion(
+        infraccion.descrip,
+        infraccion.digesto
+      );
+
+      if (matchingInfraccion) {
+        await Secuestros_infracciones.create({
+          secuestro_id: newSecuestro.id,
+          infraccion_id: matchingInfraccion.id,
+        });
+      } else {
+        console.warn(
+          `Infracción no encontrada o no cumple el umbral: ${infraccion.descrip}, ${infraccion.digesto}`
+        );
+      }
+    }
+
   
     return newSecuestro;
   } catch (error) {
