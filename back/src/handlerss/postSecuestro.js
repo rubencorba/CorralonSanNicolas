86de51712834f47083194d05fecf73e4594aefb0
rubencorba <rubencorba@gmail.com>
2025-01-14 22:41:51 -0300

    postSecuestro completo  desde Nuevo oficio Policial

--- back/src/handlerss/postSecuestro.js
+++ back/src/handlerss/postSecuestro.js
@@ -1,4 +1,11 @@
-const { Secuestros, Vehiculos,Infractores,Actas,Infracciones,Secuestros_infracciones } = require("../db.js");
+const {
+  Secuestros,
+  Vehiculos,
+  Infractores,
+  Actas,
+  Infracciones,
+  Secuestros_infracciones,
+} = require("../db.js");
 
 const postSecuestro = async (
   infractorCuil,
@@ -19,12 +26,13 @@ const postSecuestro = async (
   actaFecha_hora,
   infracciones
 ) => {
-  const ingreso = "2345";
+  const ingreso = "2345"; // Luego cambiar esto por el correcto manejo de "ingreso"
 
   var fechaTimestamp = new Date(fecha_hora).getTime();
 
-  const isoDateString = actaFecha_hora.replace(" ", "T");
-  const actaDate = new Date(isoDateString).getTime();
+  const actaDate = actaFecha_hora
+    ? new Date(actaFecha_hora.replace(" ", "T")).getTime()
+    : null;
 
   try {
     const newSecuestro = await Secuestros.create({
@@ -34,14 +42,14 @@ const postSecuestro = async (
       inventario: nroInventario,
       sector,
     });
-  
+
     const newVehiculo = await Vehiculos.create({
       marcavh: vehiculoMarca,
       modelovh: vehiculoModelo,
       tipovh: vehiculoTipo,
       dominio: vehiculoDominio,
     });
-  
+
     const newInfractor = await Infractores.create({
       dni: infractorDni,
       nombreCompleto: infractorNombre,
@@ -49,56 +57,75 @@ const postSecuestro = async (
       cuil: infractorCuil,
     });
 
-    const newActa = await Actas.create({
-      nro: actaNro,
-      inspector: actaInspector,
-      lugar: actaLugar,
-      fecha_hora: actaDate,
-    });
-    console.log(Object.keys(newSecuestro.__proto__));
+    // Si los datos del acta están presentes, crear el acta y vincularla
+    if (actaNro || actaInspector || actaLugar || actaDate) {
+      const newActa = await Actas.create({
+        nro: actaNro,
+        inspector: actaInspector,
+        lugar: actaLugar,
+        fecha_hora: actaDate,
+      });
+
+      await newSecuestro.setActa(newActa);
+    }
+    /* console.log(Object.keys(newSecuestro.__proto__)); */ //Esto es para visualizar las funciones de newSecuestro
+
     await newSecuestro.setVehiculo(newVehiculo);
     await newSecuestro.setInfractore(newInfractor);
-    await newSecuestro.setActa(newActa);
 
-
+    //Esta función se creó para los casos en los que la infracción ingresada desde juzgado
+    //no coincide 100% con la de la db de corralón en cuanto a la textualidad.
+    //Por lo que si el digesto, y dos palabras de la decrip coinciden, ya se creará el vínculo
     const findMatchingInfraccion = async (descrip, digesto) => {
       const allInfracciones = await Infracciones.findAll({
         where: { digesto },
       });
-    
+
       for (const infraccion of allInfracciones) {
         const dbWords = infraccion.descrip.split(/\s+/);
         const inputWords = descrip.split(/\s+/);
-    
+
         const commonWords = dbWords.filter((word) => inputWords.includes(word));
-        if (commonWords.length >= 4) {
+        if (commonWords.length >= 2) {
           return infraccion; // Coincidencia aceptable encontrada
         }
       }
-    
+
       return null; // No se encontró coincidencia
     };
 
-     // Vincular infracciones
-     for (const infraccion of infracciones) {
-      const matchingInfraccion = await findMatchingInfraccion(
-        infraccion.descrip,
-        infraccion.digesto
-      );
+    // Vincular infracciones
+    for (const infraccion of infracciones) {
+      let infraccionId;
 
-      if (matchingInfraccion) {
-        await Secuestros_infracciones.create({
-          secuestro_id: newSecuestro.id,
-          infraccion_id: matchingInfraccion.id,
-        });
+      // Verificar si el objeto infracción ya tiene un ID (Ingreso policial)
+      if (infraccion.id) {
+        infraccionId = infraccion.id;
       } else {
-        console.warn(
-          `Infracción no encontrada o no cumple el umbral: ${infraccion.descrip}, ${infraccion.digesto}`
+        // Buscar la infracción coincidente al no ser un ingreso policial
+        const matchingInfraccion = await findMatchingInfraccion(
+          infraccion.descrip,
+          infraccion.digesto
         );
+
+        if (matchingInfraccion) {
+          infraccionId = matchingInfraccion.id;
+        } else {
+          console.warn(
+            `Infracción no encontrada o no cumple el umbral: ${infraccion.descrip}, ${infraccion.digesto}.
+           Se vinculará con INFRACCION NO TABULADA`
+          );
+          infraccionId = 864; // ID de "INFRACCION NO TABULADA"
+        }
       }
+
+      // Crear la relación en la tabla Secuestros_infracciones
+      await Secuestros_infracciones.create({
+        secuestro_id: newSecuestro.id,
+        infraccion_id: infraccionId,
+      });
     }
 
-  
     return newSecuestro;
   } catch (error) {
     console.error("Error al crear un nuevo secuestro:", error.message);
